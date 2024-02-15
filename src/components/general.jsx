78822578c1fe79a4ad082c464d7454ca45365cb9
Justin Lamb <justinlamb@me.com>
2024-02-15 11:36:59 -0800

    general states

--- src/components/general.jsx
+++ src/components/general.jsx
@@ -1,20 +1,30 @@
+import { useState } from "react";
 import Card from "./card";
 
 function General() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: ""
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
 
   const content = (
-    <form action="" method="get" className="cards general">
+    <form onSubmit={handleSubmit} className="general">
     <div>
       <label htmlFor="name">Name: </label>
-      <input type="text" name="name" id="name" required />
+      <input onChange={(e) => setFormData({...formData, name: e.target.value})} value={formData.name} type="text" name="name" id="name" required />
     </div>
     <div>
       <label htmlFor="email">Email: </label>
-      <input type="email" name="email" id="email" required />
+      <input onChange={(e) => setFormData({...formData, email: e.target.value})} value={formData.email} type="email" name="email" id="email" required />
     </div>
     <div>
       <label htmlFor="phone">Phone: </label>
-      <input type="tel" name="phone" id="phone" />
+      <input onChange={(e) => setFormData({...formData, phone: e.target.value})} value={formData.phone} type="tel" name="phone" id="phone" />
     </div>
     <div>
       <input type="submit" value="Save" className="button"/>
